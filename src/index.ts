21c7e5fade72354ea5d0653ae04053125adb5466
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-04-16 17:48:05 +0700

    feat: register

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { errorMiddleware } from "./middlewares/error.middleware";
-import sampleRouter from "./routes/sample.router";
+import authRouter from "./routes/auth.router";
 import cors from "cors";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/samples", sampleRouter);
+
+app.use("/auth", authRouter);
+
 app.use(errorMiddleware);
+
 app.listen(8000, () => {
   console.log(`Server running on PORT :${8000}`);
 });
